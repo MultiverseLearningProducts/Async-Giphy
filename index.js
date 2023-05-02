25ed2f6dac61effec5eca63a92d4861f8a7bc6c2
ramon-almeida <ramonalmeidabkb@gmail.com>
2023-05-02 16:38:56 +0100

    new file:   index.html going forward 	modified:   index.js 	new file:   style.css

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 require('dotenv').config();
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+
+async function getImage(query) {
+ try {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const randomIndex = Math.floor(Math.random() * 25); // generate random index value
+    const imageUrl = data.data[randomIndex].images.original.url;
+    return imageUrl;
+  } catch(error) {
+    console.error(error)
+  }
+}
+
+async function getAllImages(query) {
+    try {
+       const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+       const response = await fetch(endpoint);
+       const data = await response.json();
+       const imageUrls = [];
+       data.data.forEach((gif) => {
+         imageUrls.push(gif.images.original.url);
+       });
+       console.log(imageUrls); 
+       return imageUrls;
+     } catch(error) {
+       console.error(error)
+     } 
+}
+
+getAllImages('beach')
