f7547c3e1fb80827427cd4ad1dbe710ff6a9304e
L-sampson <lavonsampson@google.com>
2023-10-05 15:01:32 -0400

    Steps 1-3 of getImage(query) function

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
-
+async function getImage(query){
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`
+    const response = await fetch(endpoint);
+    const giphy = await response.json();
+    console.log(giphy);
+}
+//Test giphy image
+getImage('dogs');
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+//console.log(process.env.API_KEY)
