4aff727d07d3d80667a8a71cc51bbbd9bc5cb216
saraiesco <saraiescobar@google.com>
2023-10-05 15:27:34 -0500

    correct code

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ require('dotenv').config();
 // Print out value of API key stored in .env file
 // console.log(process.env.API_KEY)
 
-let endpoint =  `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
-
 async function getImages(query){
+    const endpoint =`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
     const response = await fetch(endpoint)
     const data = await response.json()
     const singleImg = data["data"][0]["url"]
     console.log(singleImg)
-    endpoint =`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
-    console.log(endpoint)
-    return endpoint
+    return singleImg
 }
 
-getImages('cats')
+getImages('hunterxhunter')
