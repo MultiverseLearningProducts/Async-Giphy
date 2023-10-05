219db4806605db4e30ef45b157913d2f3d208d0e
jrtweedle1 <tweedle@google.com>
2023-10-05 14:57:07 -0500

    First attempt at creating getImage function

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+
+//Create asynchronous function
+async function getImage(endpoint) {
+    const response = await fetch(endpoint);
+    // const data = await response.json();
+    // console.log(data)
+}
+
+getImage();
