a83608d8c156c7f977cead94cf03465150200cec
Arwa Sharif <arwas@google.com>
2023-10-05 15:16:00 -0500

    complete code

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log('env', process.env.API_KEY)
+
+let endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=hunterxhunter&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+async function getImages(query){
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    // console.log(data["data"][11]["url"]);
+    const url = data["data"][11]["url"];
+
+    endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+    console.log(endpoint);
+}
+
+
+getImages("dogs");
