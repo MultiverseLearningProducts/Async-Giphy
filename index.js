748a4592fd14ec898441b09171fd847991afede1
Shami Tomita <shamito@google.com>
2023-10-05 14:38:06 -0400

    add endpoint

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`
