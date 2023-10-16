8d6b2800e49c7d1b6db95e2f9c854bd83d090514
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-16 15:16:37 +0100

    completed.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-require("dotenv").config();
+const input = document.querySelector("input");
+const container = document.querySelector(".gifs");
 
 const getImages = async (query) => {
-  const apiKey = process.env.API_KEY;
+  const apiKey = "t8ur12HnhbN9XDWKVdOx2Q7c03vmCFyJ";
   const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
   const response = await fetch(endpoint);
   const data = await response.json();
-  console.log(data);
+  return data;
 };
 
-getImages("dog");
+const handleSubmit = async (event) => {
+  if (event.target.value.trim() === "") return;
+  const gifs = await getImages(event.target.value);
+  if (!gifs) {
+    container.innerHTML = `
+        <div class="loading-container">
+        <p class="loading">Loading...</p>
+        <div>
+    `;
+  } else {
+    container.innerHTML = gifs.data.map(
+      (gif) => `
+        <img class="gif" src=${gif.images.original.url} />
+    `
+    );
+  }
+};
+
+input.addEventListener("change", (event) => handleSubmit(event));
