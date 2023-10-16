ae2758f531caee6ae3cfc1949d78be546f897e2a
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-16 12:33:18 +0100

    setting up.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+const getImages = async (query) => {
+  const apiKey = process.env.API_KEY;
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  console.log(data);
+};
+
+getImages("dog");
