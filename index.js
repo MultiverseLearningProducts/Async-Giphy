9858d20ef85b005afb7dd68ea7bd4fa6e536ad34
Lissette Valle <Lissettev.valle@gmail.com>
2023-11-13 21:21:02 -0700

    added async function

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
+
+async function getImages(query){
+    const endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=Cf8dxSutvNOj8C8Bg6pm3FSo7zs2HIab&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
+    try{
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        return data;
+    }catch{
+        console.log(err);
+    }
+}
+getImages('dogs').then((imageUrl) => {
+    if (imageUrl) {
+      const imgElement = document.querySelector('#resultImg');
+      imgElement.src = imageUrl;
+    }
+  });
 console.log(process.env.API_KEY)
