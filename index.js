88b8358b09411a176470dd035fe25e08ac3a145c
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-02-12 13:40:33 -0500

    creating getImages function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`;
+console.log(endpoint);
+
+async function getImages(query) {
+  try {
+    const response = await fetch(query);
+    const data = await response.json();
+    const imgURL = data.data[0].url;
+    return imgURL;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getImages(endpoint);
