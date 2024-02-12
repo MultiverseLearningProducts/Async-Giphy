1a4113440206eb4f3ea175873c8d83eba9e224f2
eviecutrell <evelynn.cutrell@alaskaair.com>
2024-02-12 10:11:09 -0800

    index.js updated to get an image from giphy

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-require('dotenv').config();
+require('dotenv').config({path:__dirname+'/.env'});
+
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+//console.log(process.env.API_KEY)
+
+async function getImage(query){
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+    const results = await fetch(endpoint);
+    const obj = await results.json();
+    const image = obj.data[0].url;
+    
+    return image;
+
+}
+
+getImage("test");
